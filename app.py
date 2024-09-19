8af6e623d692873b57306e452bc4e565c273407c
maduzuttion <mariaezuttion@gmail.com>
2024-09-19 09:27:31 -0300

    Att 19/09

--- app.py
+++ app.py
@@ -1,34 +1,46 @@
+#importar flask e pony
 from flask import Flask, render_template, request, redirect
 from pony.orm import *
 
+#criação do banco de dados
 db = Database()
 app = Flask(__name__)
 
 db.bind(provider='sqlite', filename='plantas.db', create_db=True)
 db.generate_mapping(create_tables=True)
 
+#criação da entidade "planta" do banco de dados
 class Planta(db.Entity):
     nomedaplanta = Required(str)
     nomecientífico = Required(str)
     infonutri = Required(str)
     descricao = Required(str)
+    indicacoes = Required(str)
     receitas = Required(str)
 
+#rota principal
 @app.route("/")
 def home():
     return render_template ("homepage.html")
 
+#rota sobre nós
 @app.route("/aboutus")
 def about():
     return render_template("about.html")
 
+#rotas de pesquisa
 @app.route("/search")
 def search():
     return render_template("search.html")
 
+@app.route("/pesquisar")
+def pesquisar():
+    pesquisa=request.args.get("txtBusca")
+
+#rotas de cadastro de nova planta
 @app.route("/form")
 def form():
-    return render_template("cadastro.html")
+    return render_template("form.html")
 
 @app.route("/cadastrarplanta")
 def cadastrar():
@@ -36,8 +48,9 @@ def cadastrar():
     nomecientifico=request.args.get("cientifico")
     infonutri=request.args.get("infonutri")
     descricao=request.args.get("descricao")
+    indicacoes=request.args.get("indicacoes")
     receitas=request.args.get("receitas")
 
     with db_session:
-        p= Planta(nomeplanta=nomeplanta, nomecientifico=nomecientifico,infonutri=infonutri, descricao=descricao, receitas=receitas)
+        p=Planta(nomeplanta=nomeplanta, nomecientifico=nomecientifico,infonutri=infonutri, descricao=descricao, indicacoes=indicacoes, receitas=receitas)
         commit()
